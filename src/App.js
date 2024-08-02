4b50f0ef5572ad3e81f5ecaf96835b828d405d34
GalaletsangM <preciousmafoko@gmail.com>
2024-08-01 22:57:38 -0500

    added basic styling t

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from 'react';
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
-      Weather app
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/galaletsang-precious-mafoko-67a13919a/">
+            Galaletsang Mafoko
+          </a>{" "}
+          and is open source on{" "}
+          <a href="https://github.com/GalaletsangM/new-weather-react-app">
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Weather.css";
+export default function Weather() {
+  return (
+    <div className="weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            ></input>
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            ></input>
+          </div>
+        </div>
+      </form>
+      <h1>Katy</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="sunny"
+            ></img>
+            <span className="temperature">36</span>
+            <span className="unit">°C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: 55%</li>
+            <li>Wind: 19km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
