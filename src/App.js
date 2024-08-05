909948b2b7b07f99846f3256b8e8ae9d2eac14d4
GalaletsangM <preciousmafoko@gmail.com>
2024-08-05 15:23:59 -0500

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity = "Katy" />
         <footer>
           This project was coded by{" "}
           <a href="https://www.linkedin.com/in/galaletsang-precious-mafoko-67a13919a/">
